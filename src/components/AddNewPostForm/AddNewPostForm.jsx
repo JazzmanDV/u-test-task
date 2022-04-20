44131f99c8c8c1e6b156d830c699dc1cb01750bf
Dan <danapple2013@gmail.com>
2022-04-21 00:17:13 +0300

    Убрана лишняя кнопка "Отмена"

--- src/components/AddNewPostForm/AddNewPostForm.jsx
+++ src/components/AddNewPostForm/AddNewPostForm.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { observer } from "mobx-react-lite";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import postsStore from "../../store/postsStore.js";
 import Button from "../../components/Button/Button";
@@ -21,14 +21,8 @@ const AddNewPostForm = observer(() => {
         }
 
         postsStore.add(title, content);
-        navigate("posts", { replace: true });
+        navigate("/posts", { replace: true });
     };
-
-    const LeftButton = () => (
-        <Link to="/posts">
-            <Button>Отмена</Button>
-        </Link>
-    );
 
     const RightButton = () => (
         <Button type="submit" onClick={handleAddButtonClick}>
@@ -41,7 +35,6 @@ const AddNewPostForm = observer(() => {
             formTitle={"Добавление новой записи"}
             titleInputRef={titleInputRef}
             contentInputRef={contentInputRef}
-            LeftButton={LeftButton}
             RightButton={RightButton}
         />
     );
