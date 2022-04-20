bfc8aeda25165dbdf55d107ab9699deb8c109f35
Dan <danapple2013@gmail.com>
2022-04-20 18:51:53 +0300

    Роутинг перенесен из index в App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import PostsPage from "../pages/PostsPage/PostsPage";
+import EditPostPage from "../pages/EditPostPage/EditPostPage";
+import AddNewPostPage from "../pages/AddNewPostPage/AddNewPostPage";
 
 import styles from "./App.module.css";
 
 const App = () => {
     return (
-        <main className={styles.mainPage}>
+        <main className={styles.app}>
             <div className={styles.contentWrapper}>
-                <Outlet />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/">
+                            <Route index element={<Navigate to="posts" replace={true} />} />
+                            <Route path="*" element={<Navigate to="posts" replace={true} />} />
+                            <Route path="posts">
+                                <Route index element={<PostsPage />} />
+                                <Route path="edit">
+                                    <Route index element={<Navigate to="posts" replace={true} />} />
+                                    <Route path=":postId" element={<EditPostPage />} />
+                                </Route>
+                                <Route path="add" element={<AddNewPostPage />} />
+                            </Route>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
             </div>
         </main>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import App from "./App/App";
-import PostsPage from "./pages/PostsPage/PostsPage";
-import EditPostPage from "./pages/EditPostPage/EditPostPage";
-import AddNewPostPage from "./pages/AddNewPostPage/AddNewPostPage";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />}>
-                    <Route index element={<Navigate to="posts" replace={true} />} />
-                    <Route path="*" element={<Navigate to="posts" replace={true} />} />
-                    <Route path="posts">
-                        <Route index element={<PostsPage />} />
-                        <Route path="edit">
-                            <Route index element={<Navigate to="posts" replace={true} />} />
-                            <Route path=":postId" element={<EditPostPage />} />
-                        </Route>
-                        <Route path="add" element={<AddNewPostPage />} />
-                    </Route>
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
